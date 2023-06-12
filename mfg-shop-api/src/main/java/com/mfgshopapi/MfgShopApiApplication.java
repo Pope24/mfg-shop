1316258000994e22c5bc64e29e69a5c01ff6e2eb
Pope24 <110238804+Pope24@users.noreply.github.com>
2023-06-12 22:35:11 +0700

    init project mfg-shop

---
+++ mfg-shop-api/src/main/java/com/mfgshopapi/MfgShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.mfgshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfgShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfgShopApiApplication.class, args);
+	}
+
+}
---
+++ mfg-shop-api/src/test/java/com/mfgshopapi/MfgShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfgshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfgShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
